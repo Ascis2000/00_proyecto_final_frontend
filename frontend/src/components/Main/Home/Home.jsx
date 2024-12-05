bc5da644628384d1dbbf79dc95a78003761c3693
unknown <ascis2000@gmail.com>
2024-12-05 17:13:32 +0100

    prueba/home

--- frontend/src/components/Main/Home/Home.jsx
+++ frontend/src/components/Main/Home/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
 
 		const fetchInicio = async () => {
 			try {
-				const response = await fetch(`${API_URL}/`, {
+				const response = await fetch(`${API_URL}`, {
 				//const response = await fetch(`http://localhost:3000/`, {
 					method: "GET",
 					headers: { "Content-Type": "application/json" },
