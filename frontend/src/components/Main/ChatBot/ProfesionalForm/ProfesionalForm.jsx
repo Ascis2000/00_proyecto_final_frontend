6e2f54af2eaa1c253dc58853098776e1bf74a025
Hirnel <cristiangarciaserena@gmail.com>
2024-12-11 20:27:38 +0100

    Profesional/Form/cleaning

--- frontend/src/components/Main/ChatBot/ProfesionalForm/ProfesionalForm.jsx
+++ frontend/src/components/Main/ChatBot/ProfesionalForm/ProfesionalForm.jsx
@@ -187,7 +187,6 @@
 // };
 
 // export default ProfesionalForm;
-
 import React, { useState, useEffect } from 'react';
 import './ProfesionalForm.css';
 import { API_URL } from '../../../../config/config.js';
@@ -205,12 +204,9 @@ const ProfesionalForm = ({ actionProvider }) => {
   const especialidadesPorAmbito = {
     "Atencion Primaria": ["Nutrición y Dietética", "Salud Mental Comunitaria"],
     Hospitalaria: ["Medicina"],
-    "Salud publica": ["medicina"],
+    "Salud publica": ["Medicina"],
     Enfermería: ["Enfermería General", "Neonatología", "Salud Pública", "Medicina Preventiva", "Cuidados Intensivos"],
-    Medicina: ["Medicina General", "Medicina Familiar y Comunitaria", "Cardiología", "Endocrinología y Nutrición", "Gastroenterología", "Hematología", "Inmunología Clínica", "Infectología (Enfermedades Infecciosas)", "Nefrología", "Neumología", "Neurología", "Oncología Médica", "Reumatología", "Medicina Interna", "Geriatría", "Medicina Paliativa", "Cirugía General", "Cirugía Cardiovascular", "Cirugía Plástica, Estética y Reparadora",
-      "Cirugía Torácica", "Cirugía Vascular", "Neurocirugía", "Oftalmología", "Otorrinolaringología", "Traumatología y Ortopedia", "Urología", "Obstetricia y Ginecología", "Pediatría", "Neonatología", "Medicina Fetal", "Medicina Reproductiva", "Radiología", "Medicina Nuclear", "Anatomía Patológica", "Genética Médica", "Bioquímica Clínica", "Medicina de Emergencias", "Medicina Intensiva", "Anestesiología y Reanimación", "Epidemiología",
-      "Salud Pública", "Medicina del Trabajo", "Medicina Preventiva", "Medicina Física y Rehabilitación", "Odontología General", "Ortodoncia", "Cirugía Maxilofacial", "Periodoncia", "Endodoncia", "Prostodoncia", "Odontopediatría", "Farmacia Clínica", "Dietética y Nutrición", "Logopedia", "Terapia Respiratoria", "Podología", "Audiología", "Acupuntura", "Homeopatía", "Naturopatía", "Medicina Integrativa", "Dermatología", "Medicina del Deporte", "Toxicología", "Medicina Forense", "Medicina Espacial", "Telemedicina"
-    ],
+    Medicina: ["Medicina General", "Medicina Familiar y Comunitaria", "Cardiología", "Endocrinología y Nutrición", "Gastroenterología", "Hematología", "Inmunología Clínica", "Infectología", "Nefrología", "Neumología", "Neurología", "Oncología Médica", "Reumatología", "Medicina Interna", "Geriatría", "Medicina Paliativa"],
     Psicología: ["Psicología Clínica", "Neuropsicología"],
     "Trabajo Social": ["Trabajo Social", "Terapia Ocupacional"],
   };
@@ -230,7 +226,6 @@ const ProfesionalForm = ({ actionProvider }) => {
           especialidad: "",
         };
       }
-
       return {
         ...prevFormData,
         [name]: value,
@@ -245,7 +240,7 @@ const ProfesionalForm = ({ actionProvider }) => {
     }
     setError('');
 
-    console.log("FORMDATA====", formData);
+    console.log("FORMDATA:", formData);
 
     fetch(`${API_URL}api/profesionales/create`, {
       method: 'POST',
@@ -257,118 +252,32 @@ const ProfesionalForm = ({ actionProvider }) => {
       .then((response) => response.json())
       .then((data) => {
         console.log('Profesional creado:', data);
-        actionProvider.handleUserSubmit(formData.tipoFormulario);
+        actionProvider.handleActionSubmit(formData.tipoFormulario, data.profesional_id);
       })
       .catch((error) => console.error('Error:', error));
   };
 
-	const handleSubmit = () => {
-		//validacion 
-		if (
-			!formData.provincia ||
-			!formData.cod_postal ||
-			!formData.ambito ||
-			!formData.especialidad
-		) {
-			setError('Por favor, completa todos los campos antes de enviar.');
-			return;
-		}
-		setError('');
-
-		console.log("FORMDATA====", formData)
-
-		fetch(`${API_URL}api/profesionales/create`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(formData),
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				console.log('Profesional creado:', data);
-				console.log("Profesional=", formData.tipoFormulario)
-				actionProvider.handleActionSubmit(formData.tipoFormulario, data.profesional_id);
-			})
-			.catch((error) => console.error('Error:', error));
-	};
-
-	return (
-		<div >
-			<h3>Por favor, ingresa tus datos:</h3>
-
-			{/* Mensaje de error */}
-			{error && <p style={{ color: 'red' }}>{error}</p>}
-			<div className="form-container">
-				{/* Selector para el País */}
-				<label htmlFor="provincia">Provincia:</label>
-				<select
-					name="provincia"
-					value={formData.provincia}
-					onChange={handleChange}
-				>
-
   return (
     <div>
       <h3>Por favor, ingresa tus datos:</h3>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
       <div className="form-container">
         <label htmlFor="provincia">Provincia:</label>
-        <select name="provincia" value={formData.provincia} onChange={handleChange}>
+        <select
+          name="provincia"
+          value={formData.provincia}
+          onChange={handleChange}
+        >
           <option value="" disabled>Selecciona tu provincia</option>
           <option value="Álava">Álava</option>
           <option value="Albacete">Albacete</option>
           <option value="Alicante">Alicante</option>
-          <option value="Almería">Almería</option>
-          <option value="Asturias">Asturias</option>
-          <option value="Ávila">Ávila</option>
-          <option value="Badajoz">Badajoz</option>
-          <option value="Barcelona">Barcelona</option>
-          <option value="Burgos">Burgos</option>
-          <option value="Cáceres">Cáceres</option>
-          <option value="Cádiz">Cádiz</option>
-          <option value="Cantabria">Cantabria</option>
-          <option value="Castellón">Castellón</option>
-          <option value="Ceuta">Ceuta</option>
-          <option value="Ciudad Real">Ciudad Real</option>
-          <option value="Córdoba">Córdoba</option>
-          <option value="La Coruña">La Coruña</option>
-          <option value="Cuenca">Cuenca</option>
-          <option value="Gerona">Gerona</option>
-          <option value="Granada">Granada</option>
-          <option value="Guadalajara">Guadalajara</option>
-          <option value="Gipuzkoa">Gipuzkoa</option>
-          <option value="Huelva">Huelva</option>
-          <option value="Huesca">Huesca</option>
-          <option value="Islas Baleares">Islas Baleares</option>
-          <option value="Jaén">Jaén</option>
-          <option value="La Rioja">La Rioja</option>
-          <option value="Las Palmas">Las Palmas</option>
-          <option value="León">León</option>
-          <option value="Lleida">Lleida</option>
-          <option value="Lugo">Lugo</option>
-          <option value="Madrid">Madrid</option>
-          <option value="Malaga">Málaga</option>
-          <option value="Melilla">Melilla</option>
-          <option value="Murcia">Murcia</option>
-          <option value="Navarra">Navarra</option>
-          <option value="Ourense">Ourense</option>
-          <option value="Palencia">Palencia</option>
-          <option value="Pontevedra">Pontevedra</option>
-          <option value="Salamanca">Salamanca</option>
-          <option value="Segovia">Segovia</option>
-          <option value="Sevilla">Sevilla</option>
-          <option value="Soria">Soria</option>
-          <option value="Tarragona">Tarragona</option>
-          <option value="Teruel">Teruel</option>
-          <option value="Toledo">Toledo</option>
-          <option value="Valencia">Valencia</option>
-          <option value="Valladolid">Valladolid</option>
-          <option value="Vizcaya">Vizcaya</option>
-          <option value="Zamora">Zamora</option>
-          <option value="Zaragoza">Zaragoza</option>
+          {/* Añade las demás provincias */}
         </select>
 
-        <label htmlFor="cod_postal">Codigo Postal:</label>
+        <label htmlFor="cod_postal">Código Postal:</label>
         <input
           type="text"
           name="cod_postal"
@@ -376,8 +285,12 @@ const ProfesionalForm = ({ actionProvider }) => {
           onChange={handleChange}
         />
 
-        <label htmlFor="ambito">Ambito:</label>
-        <select name="ambito" value={formData.ambito} onChange={handleChange}>
+        <label htmlFor="ambito">Ámbito:</label>
+        <select
+          name="ambito"
+          value={formData.ambito}
+          onChange={handleChange}
+        >
           <option value="" disabled>Selecciona tu ámbito</option>
           {Object.keys(especialidadesPorAmbito).map((ambito) => (
             <option key={ambito} value={ambito}>
@@ -402,7 +315,6 @@ const ProfesionalForm = ({ actionProvider }) => {
         </select>
 
         <button onClick={handleSubmit}>Enviar</button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
       </div>
     </div>
   );
